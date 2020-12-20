1c6397b66f7c6f27608d1a31ef21790446e05ff0
Yolanda Vergara <yv.avila@gmail.com>
2020-12-20 10:47:37 +0100

    added heroes service

--- Ejercicio-heroes/heroes/src/app/components/heroes/heroes.component.ts
+++ Ejercicio-heroes/heroes/src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+  heroes: any[] = [];
 
-  constructor() { }
+  constructor(private service: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroes = this.service.getHeroes();
   }
 
 }
